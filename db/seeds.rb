4e1e3be652a696bbebfc813f7b531852a0dc795f
yiiradz <yiiradz@gmail.com>
2020-04-22 12:55:15 -0400

    seeded courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -77,12 +77,132 @@ course2.description = "Trees Galore"
 course2.credits = 3
 course2.save!
 
-plancourse2 = PlanCourse.new
-plancourse2.plan_id = plan1.id
-plancourse2.course_id = course2.id
-plancourse2.term = "Spring"
-plancourse2.year = 2020
-plancourse2.save!
+course3 = Course.new
+course3.name = "Web Apps"
+course3.designator = "CS-3220"
+course3.description = "Who won the Medal of Honor at Gettysburg!"
+course3.credits = 3
+course3.save!
+
+course4 = Course.new
+course4.name = "O\/S"
+course4.designator = "CS-3310"
+course4.description = "Forget Windows; Let's do Linux!"
+course4.credits = 3
+course4.save!
+
+course5 = Course.new
+course5.name = "Software Security"
+course5.designator = "CS-4330"
+course5.description = "buffer overflow!"
+course5.credits = 3
+course5.save!
+
+course6 = Course.new
+course6.name = "Algorithms"
+course6.designator = "CS-3410"
+course6.description = "The heart of Computer Science"
+course6.credits = 3
+course6.save!
+
+course7 = Course.new
+course7.name = "Compiler"
+course7.designator = "CS-3510"
+course7.description = "The BEST! Way Cool!"
+course7.credits = 3
+course7.save!
+
+course8 = Course.new
+course8.name = "Database"
+course8.designator = "CS-3610"
+course8.description = "What's a left join?"
+course8.credits = 3
+course8.save!
+
+course9 = Course.new
+course9.name = "Cyber Ops"
+course9.designator = "CS-4310"
+course9.description = "Attack!"
+course9.credits = 3
+course9.save!
+
+course10 = Course.new
+course10.name = "Parallel Computing"
+course10.designator = "CS-4410"
+course10.description = "Impossible."
+course10.credits = 3
+course10.save!
+
+course11 = Course.new
+course11.name = "Software Engr I"
+course11.designator = "CS-4810"
+course11.description = "Love Senior Design!"
+course11.credits = 3
+course11.save!
+
+course12 = Course.new
+course12.name = "Software Engr II"
+course12.designator = "CS-4820"
+course12.description = "Love Senior Design!!"
+course12.credits = 3
+course12.save!
+
+course13 = Course.new
+course13.name = "DLD"
+course13.designator = "EGCP-1010"
+course13.description = "Cool course with AND, OR, and NOT"
+course13.credits = 3
+course13.save!
+
+course14 = Course.new
+course14.name = "ADLD"
+course14.designator = "EGCP-3010"
+course14.description = "I AM ROBOT"
+course14.credits = 3
+course14.save!
+
+course15 = Course.new
+course15.name = "Computer Arch"
+course15.designator = "EGCP-3210"
+course15.description = "Build the Pipeline!"
+course15.credits = 3
+course15.save!
+
+course16 = Course.new
+course16.name = "Networks"
+course16.designator = "EGCP-4310"
+course16.description = "Networking is very importing for finding a job!"
+course16.credits = 3
+course16.save!
+
+course17 = Course.new
+course17.name = "Chemistry"
+course17.designator = "CHEM-1050"
+course17.description = "Bedroom Course!"
+course17.credits = 3
+course17.save!
+
+course18 = Course.new
+course18.name = "Calc I"
+course18.designator = "MATH-1710"
+course18.description = "A weedout course."
+course18.credits = 3
+course18.save!
+
+course19 = Course.new
+course19.name = "Calc II"
+course19.designator = "MATH-1720"
+course19.description = "For the few who passed Calc I"
+course19.credits = 3
+course19.save!
+
+course20 = Course.new
+course20.name = "Physics II"
+course20.designator = "PHYS-2120"
+course20.description = "Why do we take this again?"
+course20.credits = 3
+course20.save!
+
 
 plancourse1 = PlanCourse.new
 plancourse1.plan_id = plan1.id
@@ -90,6 +210,64 @@ plancourse1.course_id = course1.id
 plancourse1.term = "Fall"
 plancourse1.year = 2020
 plancourse1.save!
+
+plancourse2 = PlanCourse.new
+plancourse2.plan_id = plan1.id
+plancourse2.course_id = course2.id
+plancourse2.term = "Fall"
+plancourse2.year = 2020
+plancourse2.save!
+
+plancourse3 = PlanCourse.new
+plancourse3.plan_id = plan1.id
+plancourse3.course_id = course3.id
+plancourse3.term = "Fall"
+plancourse3.year = 2020
+plancourse3.save!
+
+plancourse4 = PlanCourse.new
+plancourse4.plan_id = plan1.id
+plancourse4.course_id = course4.id
+plancourse4.term = "Fall"
+plancourse4.year = 2020
+plancourse4.save!
+
+plancourse5 = PlanCourse.new
+plancourse5.plan_id = plan1.id
+plancourse5.course_id = course6.id
+plancourse5.term = "Spring"
+plancourse5.year = 2020
+plancourse5.save!
+
+plancourse6 = PlanCourse.new
+plancourse6.plan_id = plan1.id
+plancourse6.course_id = course7.id
+plancourse6.term = "Spring"
+plancourse6.year = 2020
+plancourse6.save!
+
+plancourse7 = PlanCourse.new
+plancourse7.plan_id = plan1.id
+plancourse7.course_id = course7.id
+plancourse7.term = "Spring"
+plancourse7.year = 2020
+plancourse7.save!
+
+plancourse8 = PlanCourse.new
+plancourse8.plan_id = plan1.id
+plancourse8.course_id = course8.id
+plancourse8.term = "Spring"
+plancourse8.year = 2020
+plancourse8.save!
+
+plancourse9 = PlanCourse.new
+plancourse9.plan_id = plan1.id
+plancourse9.course_id = course5.id
+plancourse9.term = "Fall"
+plancourse9.year = 2020
+plancourse9.save!
+
+
 
 # New Stuff
 requirement1 = Requirement.new
@@ -127,6 +305,14 @@ coursecategory1 = CourseCategory.new
 coursecategory1.course_id = course1.id
 coursecategory1.category_id = category1.id
 coursecategory1.save!
+
+CourseCatalog.create(course_id: course2.id, catalog_id: catalog1.id)
+CourseCatalog.create(course_id: course3.id, catalog_id: catalog1.id)
+CourseCatalog.create(course_id: course4.id, catalog_id: catalog1.id)
+CourseCatalog.create(course_id: course5.id, catalog_id: catalog1.id)
+CourseCatalog.create(course_id: course6.id, catalog_id: catalog1.id)
+CourseCatalog.create(course_id: course7.id, catalog_id: catalog1.id)
+CourseCatalog.create(course_id: course8.id, catalog_id: catalog1.id)
 
 #alternate syntax
 #Plan.create(user_id: user1.id, name: "Plan1")
